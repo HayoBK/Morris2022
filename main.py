a7383d16510f20e573a325bc39bac1597e65f489
hayobk <hayo.bk@gmail.com>
2022-08-26 18:17:09 -0400

    Funcionó el tener acceso a los datos de los archivos .motion!!!

--- main.py
+++ main.py
@@ -3,21 +3,41 @@
 import json
 
 import pandas as pd
-from flatten_json import flatten
-import pandas
+
+#Abrir el archivo ".motion"... aqui lo modifique ya a .json
 with open("Navi.json", "r") as read_file:
     data = json.load(read_file)
 
-#print(data)
-#print(json.dumps(data, indent=4))
-#flat_data= flatten(data)
-#print(flat_data)
-for key in flatten(data).keys():
-    print(key)
+#------------------------------------------------------------------------------------------------------
+#Ahora vamos des jsoneando el archivo para dejarlo en una base amigable para lo que es Pandas.
+#Esto está hecho ESPECIFICAMENTE para los archivos que emite
+df = pd.json_normalize(data,'motionTrackDataPerTrials')
+df['Trial'] = range(len(df))
+#Mini Codigo para cambiar orden de columnas en un DataFrame de Pandas.
+order = [4,0,1,2,3] # setting column's order
+df = df[[df.columns[i] for i in order]]
+# explode all columns with lists of dicts
+df = df.apply(lambda x: x.explode()).reset_index(drop=True)
+df.rename(columns={'positions':'P'}, inplace=True)
+cols_to_normalize = ['P']
 
-df = pd.json_normalize(data, record_path=['motionTrackDataPerTrials'], meta=['platformExists','platformPosition.x','platformPosition.y'], errors='ignore')
+# if there are keys, which will become column names, overlap with excising column names
+# add the current column name as a prefix
+normalized = list()
+for col in cols_to_normalize:
+    d = pd.json_normalize(df[col], sep='_')
+    d.columns = [f'{col}_{v}' for v in d.columns]
+    normalized.append(d.copy())
 
-
-#df = pd.json_normalize(data)
-pd.set_option('display.max_columns', None)
+# combine df with the normalized columns
+df = pd.concat([df] + normalized, axis=1).drop(columns=cols_to_normalize)
+PosColumn = df.groupby(['Trial']).cumcount()
+PosCluymn = pd.Series(PosColumn)
+df = df.assign(Positions = PosCluymn)
+order = [0,7,4,5,6,1,2,3] # setting column's order
+df = df[[df.columns[i] for i in order]]
 print(df)
+#------------------------------------------------------------------------------------------------------
+
+
+df.to_excel('Output1.xlsx')
