b17df8ee37a8cc98b4a9f57b03a898dd6c02b866
HayoBK <hbreinbauer@uchile.cl>
2022-09-12 11:24:43 -0300

    con crédito

--- procesarDF.py
+++ procesarDF.py
@@ -6,6 +6,8 @@
 
 import pandas as pd     #Base de datos
 import numpy as np
+import matplotlib.pyplot as plt    #Graficos
+import seaborn as sns   #Estetica de gráficos
 
 m_df = pd.read_csv('MergedDataFrame.csv', index_col=0)
 m_df.rename(columns = {'platformPosition.x':'platformPosition_x', 'platformPosition.y':'platformPosition_y'}, inplace = True)
@@ -26,21 +28,61 @@ for row in m_df.itertuples():
         time = row.P_timeMilliseconds
         x = row.P_position_x
         y = row.P_position_y
+        path_length = 0
         plat = row.platformExists
         platX = row.platformPosition_x
-        playY = row.platformPosition_y
+        platY = row.platformPosition_y
+
+        #Cálculo de CSE
+        Time_Track = 100
+        x_Track = x
+        y_Track = y
+        Measures_Track = 1
+        CSE = 0
+
     if AssesedTrial != row.Trial_Unique_ID:  #Luego de que terminó el unique trial (hay que repetir esto al final del Loop.
         FirstRow = True
-        rowList = [S,M,TB,TT,ID,time,plat] #x,y,plat,platX,playY]
+        rowList = [S,M,TB,TT,ID,time,path_length,CSE,plat] #x,y,plat,platX,playY]
         MegaList.append(rowList)
+
     # y aquí ponemos lo que ocurre en cada linea intermedia
     time=row.P_timeMilliseconds # voy updateando time para que quede "max" al terminar
+    path_length+=((x - row.P_position_x)**2 + (y - row.P_position_y)**2)**0.5
+    x = row.P_position_x
+    y = row.P_position_y
 
-rowList = [S,M,TB,TT,ID,time,plat] #x,y,plat,platX,playY]
+    # Cálculo de CSE
+    x_Track += x
+    y_Track += y
+    Measures_Track += 1
+
+    if time > Time_Track:
+        Time_Track += 100
+        AvgX = x_Track / Measures_Track
+        AvgY = y_Track / Measures_Track
+        CSE += ((AvgX - platX)**2 + (AvgY - platY)**2)**0.5
+
+
+#Aqui tenemos que repetir el "rowList" para dar cuenta del ultimo trial del loop
+rowList = [S,M,TB,TT,ID,time,path_length,CSE,plat] #x,y,plat,platX,playY]
 MegaList.append(rowList)
 
-short_df = pd.DataFrame(MegaList, columns = ['Sujeto','Modalidad','True_Block','True_Trial','Trial_Unique_ID','Duration(ms)','Platform_exists'])
+short_df = pd.DataFrame(MegaList, columns = ['Sujeto','Modalidad','True_Block','True_Trial','Trial_Unique_ID','Duration(ms)','Path_length','CSE','Platform_exists'])
 print(short_df)
+
+#Hacer primer barrido para elminar los trials que hay que borrar
+Banish_List=[]
+for row in short_df.itertuples():
+    if row.Path_length < 0.0005:
+        Banish_List.append(row.Trial_Unique_ID)
+print(Banish_List)
+
+Subj='P06'
+Mod ='Realidad Virtual'
+Plat=True
+show_df = short_df.loc[ (short_df['Sujeto']==Subj) & (short_df['Modalidad']==Mod)]
+ax = sns.barplot(x="True_Block", y="Path_length",hue="True_Trial",data=show_df)
+plt.show()
 #Sujeto
 #Modalidad
 #True Block
