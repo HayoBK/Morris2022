c5162337cb140534837f61cb547b0b3e157d73b7
hayobk <hayo.bk@gmail.com>
2022-12-07 10:47:43 -0300

    Check

--- SRQ.py
+++ SRQ.py
@@ -16,5 +16,12 @@ import matplotlib.pyplot as plt    #Graficos
 data = pd.read_csv('SRQ_2022_12_07.csv', index_col=0)
 # Aqui leo el CSV que descargué de Google Forms. Ojo que le cambié el nombre para dejar marcada
 # la fecha en que lo bajé.
+data.columns.values[0]='PXX'
+PXX = data.pop('PXX')
+data = data.replace(regex=[r'\D+'], value="").astype(int)
+
+data.insert(0, 'PXX', PXX)
+
+#Aqui eliminé todos los caracters no númericos de la base de datos. Me quedo solo con los numeros.
 
 print(data)
